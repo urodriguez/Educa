a654617c5808ee4a75a099a28b2dc7b181a82359
urodriguez <ucielrodriguez1990@gmail.com>
2016-09-02 22:42:05 -0300

    Modelo basico para visualizar cursos mas completo

--- app/src/main/java/com/example/uciel/educa/Cursos.java
+++ app/src/main/java/com/example/uciel/educa/Cursos.java
@@ -1,9 +1,12 @@
 package com.example.uciel.educa;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.*;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +17,10 @@ import android.widget.TextView;
 public class Cursos extends AppCompatActivity implements SearchView.OnQueryTextListener {
     LinearLayout linearLayoutCursos;
     SearchView searchView;
+    Button btnFiltrar;
+
+    final CharSequence categorias[] = new CharSequence[] {"Nombre", "Categoria"};
+    String filtrandoPor = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +28,37 @@ public class Cursos extends AppCompatActivity implements SearchView.OnQueryTextL
         setContentView(R.layout.activity_cursos);
 
         searchView = (SearchView) findViewById(R.id.searchView);
-
         searchView.setOnQueryTextListener(this);
 
+        btnFiltrar = (Button) findViewById(R.id.buttonFiltrar);
+        btnFiltrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Cursos.this);
+                builder.setTitle("Selecciona un filtro");
+                builder.setItems(categorias, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        searchView.setQueryHint("Buscar por " + categorias[which]);
+                        filtrandoPor = categorias[which].toString();
+                    }
+                });
+                builder.show();
+            }
+        });
 
         linearLayoutCursos = (LinearLayout) findViewById(R.id.LLcursos);
 
+        this.mostrarCursos();
+    }
+
+    private void mostrarCursos() {
         TextView tva = new TextView(this);
-        tva.setText("CURSO1");
+        tva.setText("CURSO 1");
         linearLayoutCursos.addView(tva);
 
         TextView tvs = new TextView(this);
-        tvs.setText("CURSO2");
+        tvs.setText("CURSO 2");
         linearLayoutCursos.addView(tvs);
 
         ImageView img = new ImageView (this);
@@ -57,12 +83,17 @@ public class Cursos extends AppCompatActivity implements SearchView.OnQueryTextL
             }
         });
 
+        for(int i = 5; i < 150; i++){
+            TextView txt = new TextView(this);
+            txt.setText("CURSO " + String.valueOf(i));
+            linearLayoutCursos.addView(txt);
+        }
     }
 
     @Override
     public boolean onQueryTextSubmit(String query) {
         // User pressed the search button
-        android.util.Log.d("INFO", query);
+        android.util.Log.d("INFO", "Buscando: " + query + " - Filtro: " + filtrandoPor);
         return false;
     }
 
--- app/src/main/java/com/example/uciel/educa/DescripcionCurso.java
+++ app/src/main/java/com/example/uciel/educa/DescripcionCurso.java
@@ -1,13 +1,37 @@
 package com.example.uciel.educa;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.*;
+import android.view.View;
+import android.widget.Button;
 
 public class DescripcionCurso extends AppCompatActivity {
+
+    Button btnInscribirse, btnAtras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_descripcion_curso);
+
+        btnInscribirse = (Button) findViewById(R.id.buttonInscribirse);
+        btnAtras = (Button) findViewById(R.id.buttonAtras);
+
+        btnInscribirse.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                android.util.Log.d("MSG: ", "TE HAS INSCRIPTO");
+            }
+        });
+
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent cursosIntent = new Intent(DescripcionCurso.this,Cursos.class);
+                startActivity(cursosIntent);
+            }
+        });
     }
 }
