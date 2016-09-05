12e2cdf2e018d31a301f42ee364ff9f4e6d3816c
urodriguez <ucielrodriguez1990@gmail.com>
2016-09-05 03:48:52 -0300

    #5.3.1.6 redimension de imagenes para cursos y menu lateral

--- app/src/main/java/com/example/uciel/educa/Cursos.java
+++ app/src/main/java/com/example/uciel/educa/Cursos.java
@@ -3,22 +3,32 @@ package com.example.uciel.educa;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.*;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Cursos extends AppCompatActivity implements SearchView.OnQueryTextListener {
     LinearLayout linearLayoutCursos;
     SearchView searchView;
     Button btnFiltrar;
+    ListView listView;
+    DrawerLayout drawerLayout;
 
+    final String[] opciones = {"Cursos disponibles", "Mis cursos", "Diplomas"};
     final CharSequence categorias[] = new CharSequence[] {"Nombre", "Categoria"};
     String filtrandoPor = "";
 
@@ -27,6 +37,17 @@ public class Cursos extends AppCompatActivity implements SearchView.OnQueryTextL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cursos);
 
+        this.cargarMenuLateral();
+
+        this.cargarFiltroYBusqueda();
+
+        linearLayoutCursos = (LinearLayout) findViewById(R.id.LLcursos);
+
+        this.mostrarCursos();
+    }
+
+
+    private void cargarFiltroYBusqueda(){
         searchView = (SearchView) findViewById(R.id.searchView);
         searchView.setOnQueryTextListener(this);
 
@@ -46,23 +67,77 @@ public class Cursos extends AppCompatActivity implements SearchView.OnQueryTextL
                 builder.show();
             }
         });
+    }
 
-        linearLayoutCursos = (LinearLayout) findViewById(R.id.LLcursos);
+    private void cargarMenuLateral() {
+        listView = (ListView) findViewById(R.id.list_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        this.mostrarCursos();
+        listView.setAdapter(new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1,
+                opciones));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView arg0, View arg1, int arg2,
+                                    long arg3) {
+                if(arg2 == 0){
+                    Intent cursosIntent = new Intent(Cursos.this,Cursos.class);
+                    startActivity(cursosIntent);
+                } else {
+                    Toast.makeText(Cursos.this, "Mostrando... " + opciones[arg2],
+                            Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawers();
+                }
+
+            }
+        });
+    }
+
+
+    /*
+     * mostramos/ocultamos el menu al precionar el icono de la aplicacion
+     * ubicado en la barra XXX
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (drawerLayout.isDrawerOpen(listView)) {
+                    drawerLayout.closeDrawers();
+                } else {
+                    drawerLayout.openDrawer(listView);
+                }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void mostrarCursos() {
-        TextView tva = new TextView(this);
+/*        TextView tva = new TextView(this);
         tva.setText("CURSO 1");
         linearLayoutCursos.addView(tva);
 
         TextView tvs = new TextView(this);
         tvs.setText("CURSO 2");
-        linearLayoutCursos.addView(tvs);
+        linearLayoutCursos.addView(tvs);*/
+
+
+        View separador = new View(this);
+        separador.setMinimumHeight(1);
+        separador.setBackgroundColor(Color.GRAY);
+        linearLayoutCursos.addView(separador);
+
+        TextView cat1 = new TextView(this);
+        cat1.setText("PROGRAMACION");
+        linearLayoutCursos.addView(cat1);
 
         ImageView img = new ImageView (this);
-        img.setImageResource(R.drawable.test2);
+        img.setImageResource(R.drawable.angular);
+        //LinearLayout marco = new LinearLayout(this);
+        //marco.setOrientation(LinearLayout.VERTICAL);
+        img.setLayoutParams(new LinearLayout.LayoutParams(150,150));
         linearLayoutCursos.addView(img);
         img.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -72,8 +147,10 @@ public class Cursos extends AppCompatActivity implements SearchView.OnQueryTextL
             }
         });
 
+
         ImageView img2 = new ImageView (this);
-        img2.setImageResource(R.drawable.test3);
+        img2.setImageResource(R.drawable.cursojs);
+        img2.setLayoutParams(new LinearLayout.LayoutParams(150,150));
         linearLayoutCursos.addView(img2);
         img2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -83,11 +160,38 @@ public class Cursos extends AppCompatActivity implements SearchView.OnQueryTextL
             }
         });
 
-        for(int i = 5; i < 150; i++){
+
+        View separador2 = new View(this);
+        separador2.setMinimumHeight(1);
+        separador2.setBackgroundColor(Color.GRAY);
+        linearLayoutCursos.addView(separador2);
+
+        TextView cat2 = new TextView(this);
+        cat2.setText("EXACTAS");
+        linearLayoutCursos.addView(cat2);
+
+        ImageView img3 = new ImageView (this);
+        img3.setImageResource(R.drawable.matematica);
+        img3.setLayoutParams(new LinearLayout.LayoutParams(150,150));
+        linearLayoutCursos.addView(img3);
+        img3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                android.util.Log.d("INFO", "MOSTRANDO CURSO4");
+                Intent descripcionCursoIntent = new Intent(Cursos.this,DescripcionCurso.class);
+                startActivity(descripcionCursoIntent);
+            }
+        });
+
+/*        for(int i = 5; i < 100; i++){
             TextView txt = new TextView(this);
             txt.setText("CURSO " + String.valueOf(i));
             linearLayoutCursos.addView(txt);
-        }
+
+            View separadorr = new View(this);
+            separadorr.setMinimumHeight(1);
+            separadorr.setBackgroundColor(Color.GRAY);
+            linearLayoutCursos.addView(separadorr);
+        }*/
     }
 
     @Override
